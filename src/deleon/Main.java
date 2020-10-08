f38cddd2230fbbfa9844fca2e5b5f6afdcdfda94
shakerbaker78 <bakeamym@yahoo.com>
2020-10-08 12:06:55 -0600

    added my name

--- src/deleon/Main.java
+++ src/deleon/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.print("Hello, my name is Megan Nicolle Sigüenza McCoun de De León");
+	System.out.println("Hi, Megan, my name is Amy");
     }
 }
