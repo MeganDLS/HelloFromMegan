5af209c9f6f9341c7e91ce81da90b67da41072a1
wneves <wneves@byui.edu>
2020-10-09 14:21:04 -0600

    wanderson modified it

--- src/deleon/Main.java
+++ src/deleon/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.print("Hello, my name is Megan Nicolle Sigüenza McCoun de De León");
         System.out.println("Hi, Megan, my name is Amy");
         System.out.println("Hi, I am Timothy, FROM THE intelj.");
+        System.out.println("Você recebeu uma mensagem em português. Wanderson.");
     }
 }
