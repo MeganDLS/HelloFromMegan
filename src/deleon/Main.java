fd390da94b3280f1f2cf8296728919481260fed9
timjo6789 <tgj2u2@gmail.com>
2020-10-09 14:08:52 -0600

    adding timothy's code.

--- src/deleon/Main.java
+++ src/deleon/Main.java
@@ -3,8 +3,8 @@ package deleon;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello, my name is Megan Nicolle Sigüenza McCoun de De León");
-	System.out.println("Hi, Megan, my name is Amy");
-        System.out.println("Hello from Timothy.");
+        System.out.print("Hello, my name is Megan Nicolle Sigüenza McCoun de De León");
+        System.out.println("Hi, Megan, my name is Amy");
+        System.out.println("Hi, I am Timothy, FROM THE intelj.");
     }
 }
